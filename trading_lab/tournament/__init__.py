f0a952f48accb4bcc3f468b86b4a64d42d5ac91f
mojtabashariatzade <mojtaba.shariatzade@gmail.com>
2026-09-21 22:04:23 +0300

    feat(tournament): export T004 tournament API

---
+++ trading_lab/tournament/__init__.py
@@ -0,0 +1,22 @@
+"""Auditable synthetic research tournament."""
+from .core import (
+    DatasetClass,
+    Opportunity,
+    RuleBasedDecisionCore,
+    TournamentConfig,
+    TournamentDecision,
+    TournamentRun,
+    TournamentRunner,
+    default_experts,
+)
+
+__all__ = [
+    "DatasetClass",
+    "Opportunity",
+    "RuleBasedDecisionCore",
+    "TournamentConfig",
+    "TournamentDecision",
+    "TournamentRun",
+    "TournamentRunner",
+    "default_experts",
+]
